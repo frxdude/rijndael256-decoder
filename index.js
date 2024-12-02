ccba68901889d9827534085223c8ae1101d9b7d3
sainjargalishdorj <goodaberg103@gmail.com>
2024-12-02 14:47:13 +0800

    encryption added

--- index.js
+++ index.js
@@ -25,26 +25,27 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-    const { encryptedText } = req.query;
 
-    if (!encryptedText) {
-        return res.status(400).send('Missing body');
-    }
+app.get("/", (req, res) => {
+    const { text, encrypt } = req.query;
+
+    if (!text)
+        return res.status(400).send('Missing param');
 
     try {
-        const buf = Buffer.from(encryptedText.toString(), 'base64');
-        
-        if (buf.length % 32 !== 0) {
-            return res.status(400).send('Invalid encryptedText format');
+        if (encrypt === 'true') {
+            const plaintextBuffer = Buffer.from(text, 'utf8');
+            const encryptedBuffer = cipher.encrypt(plaintextBuffer, 256, iv);
+            return res.send(Buffer.from(encryptedBuffer).toString('base64'));
         }
 
-        const plaintextBuffer = Buffer.from(cipher.decrypt(buf, 256, iv));
-
-        const plaintext = plaintextBuffer.toString();
-        res.send(plaintext);
+        const encryptedBuffer = Buffer.from(text, 'base64');
+        if (encryptedBuffer.length % 32 !== 0)
+            return res.status(400).send('Invalid text format');
+        const decryptedBuffer = Buffer.from(cipher.decrypt(encryptedBuffer, 256, iv));
+        return res.send(decryptedBuffer.toString('utf8')); 
     } catch (error) {
-        console.error("Decryption error:", error.message);
-        res.status(500).send('Decryption failed');
+        console.error("Encryption/Decryption error:", error.message);
+        res.status(500).send('Operation failed');
     }
 });
